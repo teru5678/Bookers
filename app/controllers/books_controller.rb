5da9734056f3a9cf7935b9dc971ce49e63333e0b
TerukiToyonaga <127729072+teru5678@users.noreply.github.com>
2023-04-04 09:16:01 +0000

    [Add]bookのeditページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
    @book = Book.new(book_params)
    @book.save
-   redirect_to @book_path(book.id)
+   redirect_to book_path(@book)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/books' =>'books#index'
   post 'books' =>'books#create'
   get 'books/:id' => 'books#show',as:'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'book#edit',as:'edit_'
   get 'books/new'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
